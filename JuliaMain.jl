73e863bc4b35a04f8ea78eb73965d64b92a75f15
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2019-06-28 17:46:54 -0500

    Debug and performance trace code
    * debug the code
    * timing and memory
    * profiles the code
    * advanced tool for diagnosing type-related problems

--- JuliaMain.jl
+++ JuliaMain.jl
@@ -1,20 +1,18 @@
 cd(dirname(@__FILE__))
-
 clearconsole();
 
 # ]add BenchmarkTools
 # ]add MAT
 using BenchmarkTools # for benchmark
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 700;
-BenchmarkTools.DEFAULT_PARAMETERS.evals = 1;
+const BenchmarkTools.DEFAULT_PARAMETERS.samples = 700;
+const BenchmarkTools.DEFAULT_PARAMETERS.evals = 1;
 using DelimitedFiles # for readdlm, writedlm
 using Statistics # for median
 using LinearAlgebra # for eigen, pinv, svd, cholesky
 using Random # for randperm
 using MAT
 
-
-operationMode = 2; # 0 for test only # 1 for partial benchmark # 2 for full benchmark
+const operationMode = 2; # 0 for test only # 1 for partial benchmark # 2 for full benchmark
 
 # Julia
 include("JuliaBench.jl");
@@ -35,3 +33,25 @@ writedlm("RunTimeData\\RunTimeJulia$(BLAS.vendor())SIMDTable.csv", tRunTime,',')
 file = matopen("RunTimeData\\RunTimeJulia$(BLAS.vendor())SIMD.mat", "w")
 write(file, "mRunTime", mRunTime)
 close(file)
+
+# # Debug and performance trace
+# include("JuliaBench.jl");
+# matrixSize = 20;
+# mX = randn(matrixSize, matrixSize);
+# mY = randn(matrixSize, matrixSize);
+# allFunctions = [MatrixGeneration, MatrixAddition, MatrixMultiplication, MatrixQuadraticForm, MatrixReductions, ElementWiseOperations, MatrixExp, MatrixSqrt, Svd, Eig, CholDec, MatInv, LinearSystem, LeastSquares, CalcDistanceMatrix, KMeans];
+# # debug the code
+# # Juno.@enter allFunctions[2](matrixSize, mX, mY) # copy paste to REPL
+# # timing and memory
+# @time allFunctions[2](matrixSize, mX, mY)
+# # bad practices used in coding
+# # Juno.@trace allFunctions[1](matrixSize, mX, mY)
+# # profiles the code
+# # using Profile
+# # Profile.clear();
+# # @profile allFunctions[2](matrixSize, mX, mY)
+# # Profile.print(combine = true)
+# # profiletree=Juno.profiletree()
+# # Juno.profiler()
+# # advanced tool for diagnosing type-related problems
+# # @code_warntype allFunctions[2](matrixSize, mX, mY)
